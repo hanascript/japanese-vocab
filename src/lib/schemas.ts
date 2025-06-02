b765b206d2eabb752707f98e24b66d33ea10f9d5
Nathan Marcellous <taboodog@hotmail.com>
2025-06-02 03:05:03 -0700

    fix: updated schema to match db

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -2,13 +2,14 @@ import z from 'zod';
 
 export const newDeckSchema = z.object({
   name: z.string().min(1),
-  description: z.string().min(1),
-  flashcards: z.array(
-    z.object({
-      front: z.string().min(1),
-      frontSecondary: z.string().min(1),
-      back: z.string().min(1),
-      backSecondary: z.string().min(1),
-    })
-  ),
+  description: z.string().optional(),
+  flashcards: z.array(z.lazy(() => newFlashcardSchema)),
+});
+
+export const newFlashcardSchema = z.object({
+  kanji: z.string().optional(),
+  kana: z.string().min(1),
+  meaning: z.string().min(1),
+  pronunciation: z.string().optional(),
+  example: z.string().optional(),
 });
